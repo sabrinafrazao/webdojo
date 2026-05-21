ee99b5e35d662db50e252bde5238c1290342d5ff
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2026-05-20 20:26:17 -0400

    Arlets do navegador

---
+++ web/cypress/e2e/alerts.cy.js
@@ -0,0 +1,59 @@
+describe('Validações de alertas em JavaScript', () => {
+
+    beforeEach(() => {
+        cy.login()
+        cy.goTo('Alertas JS', 'JavaScript Alerts')
+    })
+
+    it('Deve validar a mensagem', () => {
+
+        cy.on('window:alert', (msg) => {
+            expect(msg).to.equal('Olá QA, eu sou uma Alert Box!')
+        })
+
+        cy.contains('button', 'Mostrar Alert').click()
+    })
+
+    it('Deve confirmar um dialogo e validar a resposta positiva', () => {
+
+        cy.on('window:confirm', (msg) => {
+            expect(msg).to.equal('Aperta um botão!')
+            return true // true simula o click no botão ok
+        })
+
+        cy.on('window:alert', (msg) => {
+            expect(msg).to.equal('Você clicou em Ok!')
+        })
+
+        cy.contains('button', 'Mostrar Confirm').click()
+    })
+
+    it('Deve cancelar um dialogo e validar a resposta negativa', () => {
+
+        cy.on('window:confirm', (msg) => {
+            expect(msg).to.equal('Aperta um botão!')
+            return false // false simula o click no botão cancel
+        })
+
+        cy.on('window:alert', (msg) => {
+            expect(msg).to.equal('Você cancelou!')
+        })
+
+        cy.contains('button', 'Mostrar Confirm').click()
+    })
+
+    it.only('Deve interagir com um prompt, inserir um texto e validar uma mensagem', () => {
+
+        cy.window().then((win) => {
+            cy.stub(win, 'prompt').returns('Sabrina')
+        })
+
+        cy.on('window:alert', (msg) => {
+            expect(msg).to.equal('Olá Sabrina! Boas vindas ao WebDojo!')
+        })
+
+        cy.contains('button', 'Mostrar Prompt').click()
+
+    })
+
+})
